08ba750442c09d6047b034f754974a5c1ea6c4df
shivam <shivamsisodia8656816@gmail.com>
2022-06-10 17:45:44 +0530

    added

---
+++ src/components/imageCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+  }
+  componentDidMount() {
+    console.log(this.imageRef);
+    setInterval(3000);
+    console.log(this.imageRef.current.clientHeight);
+  }
+  render() {
+    const { description, urls } = this.props.image;
+    return (
+      <div>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
+      </div>
+    );
+  }
+}
+export default ImageCard;
--- src/components/imageList.js
+++ src/components/imageList.js
@@ -1,15 +1,10 @@
 import React from "react";
 import "./imageList.css";
+import ImageCard from "./imageCard";
 const imageList = (props) => {
   console.log(props.images);
   const images = props.images.map((image) => {
-    return (
-      <img
-        alt={image.alt_description}
-        key={image.id}
-        src={image.urls.regular}
-      />
-    );
+    return <ImageCard key={image.id} image={image} />;
   });
   return <div className='image-list'>{images}</div>;
 };
